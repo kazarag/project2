86bb8acbe8f684e033b8619be4407e952ec7d446
kazarag <108038002+kazarag@users.noreply.github.com>
2023-06-06 10:33:23 +0700

    Add files via upload

---
+++ Cleancode.cs
@@ -0,0 +1,10 @@
+ /**
+  * Calculates the sum of two numbers.
+  *
+  * @param {number} num1 - The first number to be added.
+  * @param {number} num2 - The second number to be added.
+  * @returns {number} The sum of the two numbers.
+  */
+ function add(num1, num2) {
+   return num1 + num2;
+ }
